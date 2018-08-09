1411dc4f13ad0d8bdc8ecff2e6d8c31890ee67e6
Jorge González <iksaku@me.com>
2018-08-09 00:08:45 -0500

    Finished Number to Name :ok_hand:

--- Homework1/NumberNames.py
+++ Homework1/NumberNames.py
@@ -1,1 +1,22 @@
+from Homework1 import *
 
+
+def to_int(n):
+    try:
+        return int(n)
+    except ValueError:
+        raise InvalidNumberException
+
+
+nn = NamedNumbers()
+print('Para salir utilice: Crtl + C')
+while True:
+    try:
+        i = to_int(input('Porfavor ingrese un numero entre 0 (Cero) y 1 Millon: '))
+        print((' ' * (7 - len(str(i)))) + str(i) + ' - ' + nn.get(i))
+    except InvalidNumberException:
+        print('[Error] Porfavor ingrese un numero valido.')
+    except NumberOutOfRangeException:
+        print('[Error] Porfavor ingrese un numero mayor o igual a 0 (Cero) y menor o igual a 1 Millon.')
+    except KeyboardInterrupt:
+        quit(0)
---
+++ Homework1/README.md
@@ -0,0 +1,3 @@
+# Investigation Resources:
+
+ - [Number names in Spanish](https://es.wikipedia.org/wiki/Anexo:Nombres_de_los_n%C3%BAmeros_en_espa%C3%B1ol#Centenas)
